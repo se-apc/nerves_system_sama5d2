42589aff8c077f6613c5461dbfdcc8b6de076bb1
Frank Hunleth <fhunleth@troodon-software.com>
2018-10-09 23:15:29 -0400

    Bring system up to date (untested)

--- mix.exs
+++ mix.exs
@@ -1,30 +1,57 @@
-defmodule NervesSystemSAMA5.Mixfile do
+defmodule NervesSystemSAMA5.MixProject do
   use Mix.Project
 
+  @app :nerves_system_sama5
   @version Path.join(__DIR__, "VERSION")
-    |> File.read!
-    |> String.trim
+           |> File.read!()
+           |> String.trim()
 
   def project do
-    [app: :nerves_system_sama5,
-     version: @version,
-     elixir: "~> 1.3",
-     compilers: Mix.compilers ++ [:nerves_package],
-     description: description(),
-     package: package(),
-     deps: deps(),
-     aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]]
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.6",
+      compilers: Mix.compilers() ++ [:nerves_package],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: [loadconfig: [&bootstrap/1]],
+      docs: [extras: ["README.md"], main: "readme"]
+    ]
   end
 
   def application do
     []
   end
 
+  defp bootstrap(args) do
+    System.put_env("MIX_TARGET", "bbb")
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
+  end
+
+  defp nerves_package do
+    [
+      type: :system,
+      artifact_sites: [
+        {:github_releases, "nerves-project/#{@app}"}
+      ],
+      platform: Nerves.System.BR,
+      platform_config: [
+        defconfig: "nerves_defconfig"
+      ],
+      checksum: package_files()
+    ]
+  end
+
   defp deps do
     [
-      {:nerves, "~> 0.7", runtime: false },
-      {:nerves_system_br, "~> 0.13.3", runtime: false },
-      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.11.0", runtime: false }
+      {:nerves, "~> 1.3", runtime: false},
+      {:nerves_system_br, "1.5.2", runtime: false},
+      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "1.1.0", runtime: false},
+      {:nerves_system_linter, "~> 0.3.0", runtime: false},
+      {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -35,9 +62,29 @@ defmodule NervesSystemSAMA5.Mixfile do
   end
 
   defp package do
-    [maintainers: [],
-    files: ["LICENSE", "mix.exs", "nerves_defconfig", "nerves.exs", "README.md", "VERSION", "rootfs_overlay", "fwup.conf", "cmdline.txt", "linux-4.4.defconfig", "config.txt", "post-createfs.sh"],
-     licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/se-apc/nerves_system_sama5d2"}]
+    [
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/se-apc/nerves_system_sama5d2"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "CHANGELOG.md",
+      "cmdline.txt",
+      "config.txt",
+      "fwup_include",
+      "fwup.conf",
+      "fwup-revert.conf",
+      "LICENSE",
+      "linux-4.4.defconfig",
+      "rootfs_overlay",
+      "mix.exs",
+      "nerves_defconfig",
+      "post-createfs.sh",
+      "README.md",
+      "VERSION"
+    ]
   end
 end
